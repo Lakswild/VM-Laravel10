cbcac46d2857b45e6c12104b26f70f08c4ae0476
wawaiguntang <wawaiguntang@gmail.com>
2024-03-27 14:25:17 +0700

    new

--- resources/views/outstanding/detail/index.blade.php
+++ resources/views/outstanding/detail/index.blade.php
@@ -5,13 +5,22 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-lg-12">
+        <div class="col-lg-6">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <i class="fa fa-bar-chart-o fa-fw"></i> Detail Outstanding
                     <div class="pull-right">
                         <a href="{{ route('app.outstanding.request') }}" class="btn btn-default btn-xs">Back</a>
                     </div>
+                </div>
+                <div class="panel-body">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <i class="fa fa-bar-chart-o fa-fw"></i> Room Chat
                 </div>
                 <div class="panel-body">
                 </div>
